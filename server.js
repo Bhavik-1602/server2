969435b351f658c76b8a8d5404bbaf9d06280c4d
Bhavik-1602 <bhavikvala858@gmail.com>
2025-03-27 10:47:52 +0530

    cnage

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.use(middlewares);
 server.use(router);
 
 // Start the server
-const PORT = 4000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running at http://localhost:${PORT}`);
 });
